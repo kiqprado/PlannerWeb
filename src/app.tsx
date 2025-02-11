530255b722ca739842ab47e3d69d8700248b6308
Kaique Prado <kiqprado@outlook.com>
2025-02-11 13:18:30 -0300

    feat: add confirm guest modal

--- src/app.tsx
+++ src/app.tsx
@@ -15,6 +15,8 @@ export function App() {
 
   const [ confirmTripModal, setConfirmTripModal ] = useState(false)
 
+  const [ confirmGuestModal, setConfirmGuestModal ] = useState(false)
+
   function toggleGuestsInput() {
     setGuestInput((prev) => !prev)
   }
@@ -36,6 +38,10 @@ export function App() {
 
   function modalConfirmTrip() {
     setConfirmTripModal((prev) => !prev)
+  }
+
+  function modalConfirmGuest() {
+    setConfirmGuestModal((prev) => !prev)
   }
 
   return (
@@ -214,6 +220,7 @@ export function App() {
                 />
                 <button
                   type='button'
+                  onClick={modalConfirmGuest}
                   className=' w-full bg-lime-300 hover:bg-lime-400 text-center font-medium rounded-xl py-2.5'
                 >
                   Confirmar criação da viagem
@@ -221,6 +228,48 @@ export function App() {
               </div>
             </div>
           </div>
+        )}
+
+        { confirmGuestModal && (
+          <div className='fixed inset-0 flex items-center justify-center bg-black/60'>
+
+          <div className=' w-[640px] px-5 py-6 space-y-2 bg-zinc-900 rounded-xl'>
+            <div className='flex justify-between text-zinc-50'>
+              <h2 className='text-lg'>Confirmar participação</h2>
+              <button
+                type='button'
+                onClick={modalConfirmGuest}
+              >
+                <X/>
+              </button>
+            </div>
+
+            <p className='text-zinc-400 mb-4'>Você foi convidado(a) para participar de uma viagem para <strong className='text-zinc-100'>Florianópolis, Brasil</strong> nas datas de <strong className='text-zinc-100'>16 a 27 de Agosto de 2024</strong>.</p>
+
+            <p className='text-zinc-400 mb-4'>Para confirmar sua presença na viagem, preencha os dados abaixo:</p>
+
+            <div className='space-y-2'>
+              <input 
+                type="text"
+                icon={<User/>}
+                placeholder='Seu nome completo'
+                className=' w-full bg-zinc-950 text-zinc-300 placeholder:text-zinc-400 rounded-xl py-2.5 px-4 outline-none'
+              />
+              <input 
+                type="text"
+                icon={<Mail/>}
+                placeholder='Seu e-mail'
+                className=' w-full  bg-zinc-950 text-zinc-300 placeholder:text-zinc-400 rounded-xl py-2.5 px-4 outline-none' 
+              />
+              <button
+                type='button'
+                className=' w-full bg-lime-300 hover:bg-lime-400 text-center font-medium rounded-xl py-2.5'
+              >
+                Confirmar minha presença
+              </button>
+            </div>
+          </div>
+        </div>
         )}
 
         <p className='text-zinc-500 text-center text-sm'>Ao planejar sua viagem pela plann.er você automaticamente concorda <br/>

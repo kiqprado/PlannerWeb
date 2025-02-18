711acbc4c7b1e4047a674afcbc908a82900b3c1d
Kaique Prado <kiqprado@outlook.com>
2025-02-18 13:21:50 -0300

    feat: add route and rendering logic for displaying owner-registered links during a trip - Implemented rendering logic to display the links on the screen. - Added necessary backend logic to fetch links from the database. - Ensured proper error handling for cases where no links are found.

---
+++ src/pages/trip-details/add-new-link.tsx
@@ -0,0 +1,64 @@
+import type { FormEvent } from 'react'
+
+import { X, Tag, Link2 } from 'lucide-react'
+
+interface AddNewLinkModalProps {
+  ToggleAddNewLinkModal: () => void
+  HandleAddNewLink: (e: FormEvent<HTMLFormElement>) => void
+}
+
+export function AddNewLinkModal({ 
+  ToggleAddNewLinkModal,
+  HandleAddNewLink
+ } : AddNewLinkModalProps) {
+
+  return (
+    <div className="fixed h-screen inset-0 flex items-center justify-center bg-black/60">
+      <div className='w-[492px] bg-zinc-900 rounded-xl px-5 py-6'>
+        <div className='flex justify-between text-zinc-50'>
+          <h3 className='text-lg'>Cadastrar link</h3>
+          <button
+            type='button'
+            onClick={ToggleAddNewLinkModal}
+          >
+            <X/>
+          </button>
+        </div>
+
+        <p className='text-zinc-400 text-sm mb-5'>Todos convidados podem visualizar os links importantes.</p>
+
+        <form action="" onSubmit={HandleAddNewLink} className="space-y-2">
+
+          <div className="flex gap-2 items-center bg-zinc-950 px-4 py-2 rounded-lg">
+            <Tag className="size-5 text-zinc-300"/>
+            <input 
+              type="text"
+              name='title'
+              placeholder="Título do link"
+              className="text-zinc-300 placeholder:text-zinc-400 w-full outline-none"
+            />
+          </div>
+          
+          <div className="flex gap-2 items-center bg-zinc-950 px-4 py-2 rounded-lg">
+            <Link2 className="size-5 text-zinc-300"/>
+            <input 
+              type="url"
+              name='url'
+              placeholder="URL"
+              className="text-zinc-300 placeholder:text-zinc-400 w-full outline-none"
+            />
+          </div>
+
+          <button
+            type="submit"
+            className="w-full px-4 py-2 font-medium rounded-lg bg-lime-300 hover:bg-lime-400 text-lime-950"
+          >
+            Salvar link
+          </button>
+          
+        </form>
+      </div>
+    </div>
+  )
+  
+}
---
+++ src/pages/trip-details/links-important.tsx
@@ -0,0 +1,60 @@
+import {Link2, Plus } from 'lucide-react'
+
+export interface Link {
+  title: string
+  url: string
+}
+
+export interface LinksImportantProps {
+  ToggleAddNewLinkModal: () => void
+  links: Link[]
+}
+
+export function LinksImportant({
+  ToggleAddNewLinkModal,
+  links
+} : LinksImportantProps ) {
+
+  return (
+    <div className='flex flex-col gap-6'>
+      <h3 className='text-zinc-50 text-xl'>Links importantes</h3>
+      { links.length > 0 ? (
+        links.map( link => {
+          return (
+            <div
+              key={link.title}
+              className='flex items-center justify-between'
+            >
+              <div className='flex flex-col gap-1.5'>
+                <h4 className='text-zinc-100'>{link.title}</h4>
+                <a 
+                  href={link.url}
+                  className='text-zinc-400 text-xs'
+                >
+                  {link.url}
+                </a>
+              </div>
+
+              <a href={link.url} className="size-5 text-zinc-400 cursor-pointer">
+                <Link2/>
+              </a> 
+            </div>
+          )
+        })
+      ) : (
+            <div className='flex items-center justify-between'>
+              <span className=' text-zinc-400'>Você ainda não cadastrou  nenhum link.</span>
+            </div>     
+      )}
+
+      <button
+        type='button'
+        onClick={ToggleAddNewLinkModal}
+        className='flex justify-center items-center gap-2 text-zinc-200 bg-zinc-900 hover:bg-zinc-800 rounded-lg px-5 py-2'
+      >
+        <Plus/>
+        Cadastrar novo link
+      </button>
+    </div>
+  )
+}

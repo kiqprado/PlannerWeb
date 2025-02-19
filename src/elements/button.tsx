a94e3b3e6e14bd145ab90adf797fd313d437f06c
Kaique Prado <kiqprado@outlook.com>
2025-02-19 13:20:14 -0300

    refactor: Implement button variants separation for full application refactor -Created a Button component with support for multiple variant. -Added props for size, colorScheme, and disabled states. -Implemented dynamic class generation based on props.

---
+++ src/elements/button.tsx
@@ -0,0 +1,38 @@
+import type { ComponentProps, ReactNode } from "react"
+
+import { tv, VariantProps } from 'tailwind-variants'
+
+const ButtonVariants = tv({
+  base: 'flex items-center justify-center gap-2 px-4 py-2 font-medium rounded-xl',
+  variants: {
+    variant: {
+      primary: 'bg-lime-300 hover:bg-lime-400 text-lime-950',
+      secondary: 'bg-zinc-800 hover:bg-zinc-700 text-zinc-200',
+      tertiary: 'flex-1 text-left'
+    },
+    size: {
+      full: 'w-full'
+    }
+  },
+  defaultVariants: {
+    variant: 'primary'
+  }
+
+})
+
+interface ButtonProps extends ComponentProps<'button'>,
+VariantProps<typeof ButtonVariants> {
+ children: ReactNode,
+}
+
+export function Button({ children, variant, size, ...props} : ButtonProps) {
+  return (
+    <button
+      {...props}
+      type='submit'
+      className={ButtonVariants({ variant, size })}
+    > 
+      {children}
+    </button>
+  )
+}

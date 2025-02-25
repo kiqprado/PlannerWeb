7daf9a76c546c906193933ffdc2318ecd6a9d831
Kaique Prado <kiqprado@outlook.com>
2025-02-25 16:09:10 -0300

    feat: add logic to display guest list in the trip details sidebar - Implement logic to fetch and display guest data in the trip details sidebar. - Integrate with the API to load the guest list

--- src/pages/trip-details/guest-list.tsx
+++ src/pages/trip-details/guest-list.tsx
@@ -1,40 +1,61 @@
-import { CircleDashed, UserCog } from 'lucide-react'
+import { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+
+import { api } from '../../lib/axios'
+
+import { CircleDashed, CircleCheck, UserCog } from 'lucide-react'
 
 import { Button } from '../../elements/button'
 import { ButtonIcon } from '../../elements/button-icon'
 
-export interface Guest {
-  name: string,
+export interface Participant {
+  id: string
+  name: string | null
   email: string
+  is_confirmed: boolean
 }
 
 export interface GuestsListProps {
   ToggleModalConfirmGuest: () => void
-  guests: Guest[]
+  guests: Participant[]
 }
 export function GuestsLists({
   ToggleModalConfirmGuest,
-  guests
 }: GuestsListProps) {
+  const { tripId } = useParams()
+
+  const [ participants, setParticipants ] = useState<Participant[] | undefined>()
+
+ useEffect(() => {
+  api.get(`/trips/${tripId}/participants`).then(response => setParticipants(response.data.participants))
+ }, [tripId])
+
+ if (!participants) {
+  return <div>Carregando...</div>; // Ou qualquer outro indicador de carregamento
+}
 
   return (
+
     <div className='flex flex-col gap-6'>
       <h3 className='text-zinc-50 text-xl'>Convidados</h3>
-      { guests.length > 0 ? (
-        guests.map( guest => {
+      { participants.length > 0 ? (
+        participants.map( participant => {
           return (
             <div 
-              key={guest.name}
+              key={participant.id}
               className='flex items-center justify-between'>
               <div className='flex flex-col gap-1.5'>
-                <h4 className='text-zinc-100'>{guest.name}</h4>
+                <h4 className='text-zinc-100'>{participant.name}</h4>
                 <span className='text-zinc-400 text-xs'>
-                  {guest.email}
+                  {participant.email}
                 </span>
               </div>
 
               <ButtonIcon>
-                <CircleDashed className='shrink-0'/>
+                { participant.is_confirmed ?
+                  <CircleCheck className='shrink-0 text-lime-300'/> : 
+                  <CircleDashed className='shrink-0'/>
+                }
               </ButtonIcon>
             </div>
           )
